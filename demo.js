35b883af0fd76e51ee40f16017bda70727ea02b0
Isis van der Plas <isisvanderplas@hotmail.com>
2018-04-25 17:04:56 +0200

    create server for static file

--- demo.js
+++ demo.js
@@ -1,4 +1,24 @@
 const fs = require('fs');
 const _ = require('lodash');
+// var data = require('./data.json');
 
-console.log(_.random(1,10));
+// console.log(_.random(1,10));
+//
+// console.log(data.name);
+//
+// fs.readFile('./data.json', 'utf-8', (err, data) => {
+//   var data = JSON.parse(data);
+//   console.log(data.name);
+// })
+
+// fs.readdir('/', (err, data) => {
+//   console.log(data);
+// })
+
+var data = {
+  "name": "Isis van der Plas"
+};
+
+fs.writeFile('data.json', JSON.stringify(data), (err) => {
+  console.log("write finished", (err));
+});
