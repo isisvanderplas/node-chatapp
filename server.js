155a322db93510d16b2175947b0735d0e2bb29bc
Isis van der Plas <isisvanderplas@hotmail.com>
2018-04-27 18:23:25 +0200

    add bootstrap, jquery and js

--- server.js
+++ server.js
@@ -2,6 +2,56 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname));
-let server = app.listen(3000, () => {
-  console.log('server is listening on port ', server.address().port)
+const server = app.listen(3000, () => {
+  console.log('server is listening on port', server.address().port)
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const app = express();
+//
+// app.use(express.static(__dirname));
+// let server = app.listen(3000, () => {
+//   console.log('server is listening on port ', server.address().port)
+// });
